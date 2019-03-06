6fb2aaa230f4b2de555ebca7459ed566a105c2ea
h181226 <simengjerde@hotmail.com>
2019-03-06 17:32:11 +0100

    Laget terningklassen med metoder

--- src/no/hvl/dat109/Terning.java
+++ src/no/hvl/dat109/Terning.java
@@ -1,5 +1,22 @@
 package no.hvl.dat109;
 
-public class Terning {
+import java.util.Random;
 
+public class Terning {
+	//Objektvariabler
+	private Integer verdi;
+	
+	//Konstruktør
+	public Terning() {
+		verdi = 0;
+	}
+	
+	//Metoder
+	public Integer trillTerning() {
+		Random r = new Random();
+		
+		verdi = r.nextInt(5) + 1;
+		
+		return verdi;
+	}
 }
