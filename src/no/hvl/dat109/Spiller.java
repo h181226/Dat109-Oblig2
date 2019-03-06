4863ea54af8231232fe0529d14268fa23efe08f4
h181226 <simengjerde@hotmail.com>
2019-03-06 18:06:47 +0100

    Spiller-klassen ferdig

--- src/no/hvl/dat109/Spiller.java
+++ src/no/hvl/dat109/Spiller.java
@@ -1,5 +1,51 @@
 package no.hvl.dat109;
 
 public class Spiller {
+	//Objektvariabler
+	private String navn;
+	private Integer sum;
+	
+	//Konstruktør
+	public Spiller() {
+		sum = 0;
+	}
+	
+	public Spiller(String navn) {
+		this.navn = navn;
+		sum = 0;
+	}
+	
+	//Metoder
+	/**
+	 * Triller terningene i koppen og legger deretter til verdien i sum
+	 * @param k Kopp med terninger som brukes i spillet
+	 */
+	public void spill(Kopp k) {
+		k.trill();
+		
+		sum = k.getSum();
+	}
 
+	public String getNavn() {
+		return navn;
+	}
+
+	public void setNavn(String navn) {
+		this.navn = navn;
+	}
+
+	public Integer getSum() {
+		return sum;
+	}
+
+	public void setSum(Integer sum) {
+		this.sum = sum;
+	}
+
+	@Override
+	public String toString() {
+		return "Spiller [navn=" + navn + ", sum=" + sum + "]";
+	}
+	
+	
 }
